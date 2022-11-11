182944f5529117b170bf9060a99d9743659ea679
Mohammed Raheez <42299547+mohdraheez@users.noreply.github.com>
2022-11-11 23:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var switcher = document.querySelector('.switcher');
+var logindiv = document.querySelector('.logindiv');
+var input = document.querySelectorAll('.textinput');
+
+input[0].classList.add('textinput');
+input[1].classList.add('textinput');
+input[2].classList.add('textinput');
+
+logindiv.classList.add('black_text');
+
+switcher.addEventListener('click', ()=>{
+    document.querySelector('body').classList.toggle('black');
+    logindiv.classList.toggle('dark');
+    input[0].classList.toggle('inputLight');
+    input[1].classList.toggle('inputLight');
+    input[2].classList.toggle('inputLight');
+})
+
